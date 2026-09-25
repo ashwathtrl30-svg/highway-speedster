7ecfea0f7cdf7d21bd08e065b72af9a94c1e4f99
ashwathtrl30-svg <ashwathtrl30@gmail.com>
2026-09-25 23:24:18 +0530

    Initialize App component with game UI and loading

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { GameScene } from './game/Scene'
+import {
+  HUD,
+  MainMenu,
+  PauseMenu,
+  GameOverScreen,
+  TouchControls,
+  UnlockNotification,
+  LoadingScreen,
+} from './game/UI'
+import { actions } from './game/store'
+
+function App() {
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    actions.resetGame()
+    const timer = setTimeout(() => setLoaded(true), 1500)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <div className="w-full h-full relative overflow-hidden bg-black">
+      <div className="absolute inset-0">
+        <GameScene />
+      </div>
+
+      {!loaded && <LoadingScreen />}
+      <MainMenu />
+      <HUD />
+      <PauseMenu />
+      <GameOverScreen />
+      <TouchControls />
+      <UnlockNotification />
+    </div>
+  )
+}
+
+export default App
